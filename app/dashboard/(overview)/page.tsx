58bf9613df291b793f98bc3fbb2a73e786be78b2
CMRicardo <ricardocorrales84@gmail.com>
2024-02-24 09:17:40 -0600

    feat: :sparkles: Completed chapter 16

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,10 +1,19 @@
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { CardsSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons';
+import {
+  CardsSkeleton,
+  LatestInvoicesSkeleton,
+  RevenueChartSkeleton,
+} from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import CardWrapper from '@/app/ui/dashboard/cards';
- 
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Home',
+};
+
 export default async function Page() {
   return (
     <main>
@@ -12,15 +21,15 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Suspense fallback={<CardsSkeleton />} >
-          <CardWrapper/>
+        <Suspense fallback={<CardsSkeleton />}>
+          <CardWrapper />
         </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <Suspense fallback={<RevenueChartSkeleton />} >
-          <RevenueChart  />
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
         </Suspense>
-        <Suspense fallback={<LatestInvoicesSkeleton />} >
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
           <LatestInvoices />
         </Suspense>
       </div>
